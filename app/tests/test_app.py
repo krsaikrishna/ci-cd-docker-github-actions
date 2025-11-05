291c8ce17e99b637ea85bfcc0ed337843c3c4121
krsaikrishna <krsaikrishnasrv@gmail.com>
2025-11-05 12:18:28 +0530

    Update test assertion for index route message

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -4,4 +4,5 @@ def test_index():
     client = app.test_client()
     res = client.get("/")
     assert res.status_code == 200
-    assert b"Hello from Flask in Docker!" in res.data
+    assert b"Hello from Flask CI/CD App!" in res.data
+
