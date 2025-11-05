678319169ad7f3d67394f50595062ba992ddb0f1
krsaikrishna <krsaikrishnasrv@gmail.com>
2025-11-05 11:50:47 +0530

    Rename index function to home and update message

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return jsonify(message="Hello from Flask in Docker!")
+@app.route('/')
+def home():
+    return "Hello from Flask CI/CD App!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
